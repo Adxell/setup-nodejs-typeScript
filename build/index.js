35b9e322825449aad9e7699b772b8fe85cf52c97
adxell <adxell02@gmail.com>
2022-11-13 05:20:39 -0500

    first commit setup

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Hello word');
+console.log('the best world is your eyes ');
+let number;
+number = 12;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log('Hello word');
+
+console.log('the best world is your eyes ')
+
+let number: number;
+
+
+number=12
+
